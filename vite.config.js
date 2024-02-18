6466d777cfeb89875d34d0c22caa0afe34e95cdf
bob <bob@>
2024-02-18 18:50:31 +0500

    changes

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base:"myapp",
+  base:"Portfolio",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
